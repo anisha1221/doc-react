e1b372ec67d990fdae0aeb486b7ad40ab42a1712
Manisha Bharti <manisha@Manishas-MacBook-Pro.local>
2024-06-09 17:47:18 -0500

    intial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Manisha, welcome to react!!!
+          I am changed now....
         </p>
         <a
           className="App-link"
